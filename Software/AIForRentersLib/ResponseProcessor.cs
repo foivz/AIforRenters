1cb93b10c9a8c66ebe10147fdf82c17ef2586738
Josip <47973098+josiprosandic@users.noreply.github.com>
2020-06-13 20:49:32 +0200

    ResponseProcessor implemented after being created and tested.

--- Software/AIForRentersLib/ResponseProcessor.cs
+++ Software/AIForRentersLib/ResponseProcessor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.DateTime;
+using Microsoft.Recognizers.Text.Number;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,58 @@ namespace AIForRentersLib
 {
     public static class ResponseProcessor
     {
+        /// <summary>
+        /// Receives instance (object) of ReceivedData class and doing semantic analysis on body of received email as a string.
+        /// This method is using Microsoft.Recognizers.Text library for semantic analysis.
+        /// </summary>
+        /// <param name="receivedData"></param>
+        /// <returns>
+        /// New request object of class Request with its attributes.
+        /// </returns>
         public static Request ProcessData(ReceivedData receivedData)
         {
-            return null;
+            string emailBody = ""; // = receivedData.EmailBody;
+
+            DateTime dateFrom = DateTime.Parse(ExtractDateFrom(emailBody));
+            DateTime dateTo = DateTime.Parse(ExtractDateTo(emailBody));
+            int numberOfPeople = ExtractNumberOfPeople(emailBody);
+
+            Request newRequest = new Request()
+            {
+                FromDate = dateFrom,
+                ToDate = dateTo,
+                NumberOfPeople = numberOfPeople
+            };
+
+            return newRequest;
+        }
+
+
+        private static int ExtractNumberOfPeople(string testEmailString)
+        {
+            var result = NumberRecognizer.RecognizeNumber(testEmailString, Culture.English);
+            int.TryParse(result.First().Resolution["value"].ToString(), out int value);
+            return value;
+        }
+
+        public static string ExtractDateFrom(string emailBody)
+        {
+            string dateFrom = ExtractDates(emailBody).ToList<string>().ToArray()[2];
+            return dateFrom;
+        }
+
+        public static string ExtractDateTo(string emailBody)
+        {
+            string dateTo = ExtractDates(emailBody).ToList<string>().ToArray()[3];
+            return dateTo;
+        }
+
+        public static Dictionary<string, string>.ValueCollection ExtractDates(string emailBody)
+        {
+            var result = DateTimeRecognizer.RecognizeDateTime(emailBody, Culture.English);
+            var extractFirstLayer = result.First().Resolution.Values.First() as List<Dictionary<string, string>>;
+            var dictionaryAsValueCollection = extractFirstLayer.First().Values;
+            return dictionaryAsValueCollection;
         }
     }
 }
